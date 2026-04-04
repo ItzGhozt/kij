c2c608a4786da3449e23b158b3b97821452115e9
Isabel Yeow <Itzaghoste@gmail.com>
2026-04-04 14:53:52 -0400

    fixing admin login

--- backend/main.py
+++ backend/main.py
@@ -36,12 +36,8 @@ app = FastAPI(title="KIJ Volleyball Tournament API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://kij-lime.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5500",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
